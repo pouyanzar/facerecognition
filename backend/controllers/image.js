ccd99ca8fe68e57445e83a9508979571de8b1766
Pouyan <pouyan.zarbafian@hotmail.com>
2024-01-25 22:17:51 -0500

    fix err message

--- backend/controllers/image.js
+++ backend/controllers/image.js
@@ -7,7 +7,7 @@ const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then((data) => res.json(data))
-    .catch((err) => res.status(400).json("unable to work with API"));
+    .catch((err) => res.status(400).json(err));
 };
 const handleImage = (req, res, db) => {
   const { id } = req.body;
--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,5 +1,6 @@
 const handleRegister = (req, res, db, bcrypt, saltRounds) => {
   const { email, name, password } = req.body;
+  console.log(req.body);
   if (!email || !name || !password) {
     return res.status(400).json("incorrect form submission");
   }
@@ -27,7 +28,7 @@ const handleRegister = (req, res, db, bcrypt, saltRounds) => {
         })
         .then(trx.commit)
         .catch(trx.rollback);
-    }).catch((err) => res.status(400).json("unable to register"));
+    }).catch((err) => res.status(400).json(err));
   });
 };
 
