392bef0668036ba94d1c0d0c48b380c969ba5e94
Pouyan <pouyan.zarbafian@hotmail.com>
2024-01-25 22:54:12 -0500

    update model id

--- backend/controllers/image.js
+++ backend/controllers/image.js
@@ -4,8 +4,9 @@ const app = new Clarifai.App({
   apiKey: process.env.API_CLARIFAI,
 });
 const handleApiCall = (req, res) => {
+  console.log(app.apiKey);
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict("face-detection", req.body.input)
     .then((data) => res.json(data))
     .catch((err) => res.status(400).json(err));
 };
